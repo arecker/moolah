c8b97260edfd491e4828196bc27952d572d7197b
Alex Recker <alex@reckerfamily.com>
2015-06-29 17:10:44 -0500

    Auth: Adds working base

--- moolah/settings.py
+++ moolah/settings.py
@@ -58,4 +58,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
 STATIC_URL = '/static/'
--- moolah/urls.py
+++ moolah/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import HttpResponse
+
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def test_response(request):
+    return HttpResponse('working, mate')
 
 
 urlpatterns = [
     url('^', include('django.contrib.auth.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', test_response)
 ]
