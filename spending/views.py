0c55272f3787a457cb2e2bbd513db8843013529c
Alex Recker <alex@reckerfamily.com>
2015-07-09 00:24:35 -0500

    More stuff

--- spending/views.py
+++ spending/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.template import RequestContext
+from django.contrib.auth.models import User
 from .models import MonthlyBudget, MonthlyTransaction
 
 
@@ -9,14 +10,44 @@ from .models import MonthlyBudget, MonthlyTransaction
 def budget_detail(request, pk=None):
     try:
         budget = MonthlyBudget.objects.get(pk=pk)
-        transactions = MonthlyTransaction.objects.latest_period(budget=budget)
+        if budget.shared:
+            return _return_shared_budget_detail(request, budget)
+        else:
+            return _return_individual_budget_detail(request, budget)
     except (MonthlyBudget.DoesNotExist):
         # todo: log it
         raise Http404
+
+
+def _return_individual_budget_detail(request, budget):
+    users = User.objects.all()
+    sets = []
+    for u in users:
+        transactions = MonthlyTransaction.objects.latest_period(
+            budget=budget
+        ).filter(user=u)
+        sets.append({
+            'user': u,
+            'transactions': transactions,
+            'balance': transactions.balance()
+        })
+
     return render_to_response(
-        'spending/budget_detail.html',
+        'spending/individual_budget_detail.html',
         RequestContext(request, {
             'budget': budget,
-            'transactions': transactions
+            'sets': sets
+        })
+    )
+
+
+def _return_shared_budget_detail(request, budget):
+    transactions = MonthlyTransaction.objects.latest_period(budget=budget)
+    return render_to_response(
+        'spending/shared_budget_detail.html',
+        RequestContext(request, {
+            'budget': budget,
+            'transactions': transactions,
+            'balance': transactions.balance()
         })
     )
