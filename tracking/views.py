63e4ef4a18567a047b583924a9bf4af7c4dc9c51
Alex Recker <alex@reckerfamily.com>
2015-11-02 00:04:11 -0600

    Tweaks summary page
    - adds timestamp
    - adds table

--- tracking/views.py
+++ tracking/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 
-from models import Transaction, Rate
+from models import Transaction, Rate, get_timestamp
 
 
 class Summary(TemplateView):
@@ -9,6 +9,7 @@ class Summary(TemplateView):
     def get_context_data(self, *args, **kwargs):
         transactions = Transaction.objects
         context = super(Summary, self).get_context_data(*args, **kwargs)
+        context['timestamp'] = get_timestamp()
         context['rate'] = Rate.objects.total()
         context['today'] = transactions.today().total()
         context['week'] = transactions.last_week().total()
