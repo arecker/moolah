43f78cc6b4f0fefb09405ffeff7cf1a5ebdab9f2
Alex Recker <alex@reckerfamily.com>
2015-11-01 21:14:37 -0600

    Added fancy model admins

--- tracking/admin.py
+++ tracking/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from models import Rate, Transaction
 
 
-admin.site.register(Rate)
-admin.site.register(Transaction)
+class RateAdmin(admin.ModelAdmin):
+    list_display = ('description', 'amount_per_day', 'amount', 'days')
+
+
+class TranactionAdmin(admin.ModelAdmin):
+    list_display = ('description', 'amount', 'timestamp')
+
+
+admin.site.register(Rate, RateAdmin)
+admin.site.register(Transaction, TranactionAdmin)
