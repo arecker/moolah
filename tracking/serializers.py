f307c831593e5713210b1f29e511ae38117e702e
Alex Recker <alex@reckerfamily.com>
2015-11-05 18:48:01 -0600

    Flips the sign on purchase amounts.

--- tracking/serializers.py
+++ tracking/serializers.py
@@ -3,6 +3,11 @@ from rest_framework import serializers
 from models import Transaction, Rate
 
 class TransactionSerializer(serializers.ModelSerializer):
+
+    def create(self, valid_data):
+        valid_data['amount'] = valid_data['amount'] * -1
+        return super(TransactionSerializer, self).create(valid_data)
+
     class Meta:
         model = Transaction
         exclude = ()
