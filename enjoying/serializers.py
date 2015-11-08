0e670854a554c3aff47e65ccc45410f2c07125b4
Alex Recker <alex@reckerfamily.com>
2015-11-07 19:35:53 -0600

    Adds current user to purchase

--- enjoying/serializers.py
+++ enjoying/serializers.py
@@ -4,6 +4,10 @@ from models import Transaction
 
 
 class TransactionSerializer(serializers.ModelSerializer):
+    def create(self, valid_data):  # attach current user
+        valid_data['user'] = self.context.get('request').user
+        return super(TransactionSerializer, self).create(valid_data)
+
     class Meta:
         model = Transaction
         exclude = ('user', )
