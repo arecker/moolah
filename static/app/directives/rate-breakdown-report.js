8c031659c41a4ad7ad38e6aa272a6eb03738b83c
Alex Recker <alex@reckerfamily.com>
2015-11-11 20:14:28 -0600

    Removes crap from donut chart

--- static/app/directives/rate-breakdown-report.js
+++ static/app/directives/rate-breakdown-report.js
@@ -5,25 +5,6 @@ angular.module('moolah')
 
         self.cardTitle = 'Rate breakdown';
 
-        self.options = {
-            pointDot : false,
-            pointHitDetectionRadius: 0,
-            scaleShowVerticalLines: false,
-            scaleFontSize: 0,
-            tooltipTemplate: function(obj) {
-                var label = '{} days ago'.format(obj.label),
-                    value;
-
-                if (obj.value < 0) {
-                    value = '-${}'.format(obj.value);
-                } else {
-                    value = '${}'.format(obj.value);
-                }
-
-                return '{} : {}'.format(label, value);
-            }
-        };
-
         ReportService.rateBreakdown().success(function(d) {
             self.data = d.data;
             self.labels = d.labels;
