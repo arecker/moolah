d5f009657d6b29f428d740a888fb187cf485ad50
Alex Recker <alex@reckerfamily.com>
2016-07-05 18:51:54 -0500

    Fixes celery

--- moolah/__init__.py
+++ moolah/__init__.py
@@ -1,4 +1,5 @@
-from django.conf import settings
+from __future__ import absolute_import
 
-if getattr(settings, 'BROKER_URL', None):
-    from .celery import app as celery_app
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from .celery import app as celery_app  # noqa
