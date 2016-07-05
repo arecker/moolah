def324de577c217d09a1e55f382721cb7a2a90c5
Alex Recker <arecker@healthgrades.com>
2016-07-04 21:13:25 -0500

    Adds login page

--- moolah/urls.py
+++ moolah/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
@@ -8,6 +9,7 @@ from api import ROUTER
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(ROUTER.urls)),
+    url(r'^accounts/login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
 
     url(r'^$', login_required(TemplateView.as_view(template_name='index.html')), name='home')
 ]
