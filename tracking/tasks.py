9733ffe606a39528571620888b316b72bab43f12
Alex Recker <alex@reckerfamily.com>
2016-07-07 22:46:56 -0500

    Removes test

--- tracking/tasks.py
+++ tracking/tasks.py
@@ -1,5 +1,4 @@
 from celery.task.schedules import crontab
-from celery.utils.log import get_task_logger
 from celery.decorators import periodic_task
 from django.utils import timezone
 
@@ -25,11 +24,3 @@ def compute_allowance():
             amount=allowance.amount,
             allowance=allowance
         )
-
-
-logger = get_task_logger(__name__)
-
-@periodic_task(run_every=crontab(), name="sanity", ignore_result=True)
-def sanity():
-    logger.info('Hello from moolah-celery')
-    print('Hello from moolah-celery stdout')
