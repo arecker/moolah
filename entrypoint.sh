6c1abacbc1c8c6d05eb8964e2a1f3b7a064b65a5
Alex Recker <arecker@zendesk.com>
2017-01-04 09:06:58 -0600

    Removes nginx from container

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,8 @@ python manage.py migrate --noinput
 case $1 in
     "gunicorn" )
 	cd /home/docker/src && \
-	    su -c '/usr/local/bin/gunicorn -b 0.0.0.0:8000 moolah.wsgi' docker ;;
+	    su -c '/usr/local/bin/gunicorn -b 0.0.0.0:80 moolah.wsgi' docker ;;
     "celery" )
 	cd /home/docker/src && \
 	    su -c '/usr/local/bin/celery worker -A moolah -B --concurrency=1' docker ;;
-    "nginx" )
-	nginx ;;
 esac
